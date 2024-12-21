0f2b6bf9defad802a879e9aac394b7294b2a3833
m1kowhy <primary099@gmail.com>
2024-12-21 21:28:08 +0100

    dziala

--- main.cpp
+++ main.cpp
@@ -3,13 +3,42 @@
 
 using namespace std;
 
+int wybor;
+void funkcja1();
+void funkcja2();
+void funkcja3();
+
 int main(){
 
-cout << "TEST";
-cout << "siema";
-cout << "elo";
-cout << "jak to dziala";
+    cout << "--- Menu narzedzia inzynieryjnego (idk) ---" << endl << endl;
+    cout << "1. Opcja1" << endl;
+    cout << "2. Opcja2" << endl << endl;
+    cout << "-------------------------------------------" << endl << endl;
+    cout << "Wprowadz numer opcji: ";
+    cin >> wybor;
 
+    switch (wybor){
 
+        case 1: funkcja1();
+        break;
 
+        case 2: funkcja2();
+        break;
+
+        default: funkcja3();
+        break;
+    }
+
+}
+
+void funkcja1(){
+    cout << "opcja 1 dziala" << endl;
+}
+
+void funkcja2(){
+    cout << "opcja 2 dziala" << endl;
+}
+
+void funkcja3(){
+    cout << "to nie dziala" << endl;
 }
